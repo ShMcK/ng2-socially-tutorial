26be15fecb58ffcecdc03bdb6139e76ea30466e2
ShMcK <me@shmck.com>
2015-08-23 10:53:26 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meteor-Angular2 Socially Tutorial (WIP)
+ 
+Step by step tutorial available at [Angular-Meteor.com](http://angular-meteor.com/tutorials/angular2/bootstrapping).
---
+++ packages/ng2-socially-tutorial/client/registerTutorial.js
@@ -0,0 +1,11 @@
+Diffbox.registerTutorial('tutorialAngular2', {
+  githubRepoName: 'shmck/ng2-socially-tutorial',
+  patchFilename: 'angular2Tutorial.multi.patch'
+});
+
+//TutorialRegistry.registerTutorial({
+//  title: 'Meteor Angular2 Tutorial',
+//  subtitle: 'Socially',
+//  tutorialSourceLink: 'angular-meteor.com/tutorials/angular2/bootstrapping',
+//  steps: []
+//});
