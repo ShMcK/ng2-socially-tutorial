765ca43549184c45bf9af47a6c755bb00fe23e6d
ShMcK <me@shmck.com>
2015-08-23 19:11:26 +0900

    Step 4.6: Create a partiesForm control group

--- client/parties-form/parties-form.ts
+++ client/parties-form/parties-form.ts
@@ -8,4 +8,11 @@ import {FORM_DIRECTIVES, Control, ControlGroup, Validators} from 'angular2/angul
   templateUrl: 'client/parties-form/parties-form.ng.html',
   directives: [FORM_DIRECTIVES]
 })
-export class PartiesForm {}
+export class PartiesForm {
+  constructor() {
+    this.partiesForm = new ControlGroup({
+      name: new Control(''),
+      description: new Control('')
+    });
+  }
+}
