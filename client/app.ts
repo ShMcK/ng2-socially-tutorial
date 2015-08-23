aa2a3af74e0cd2b9bacff9c48300bec88a6d44f2
ShMcK <me@shmck.com>
2015-08-23 18:55:48 +0900

    Step 3.2: Fetch parties reactively

--- client/app.ts
+++ client/app.ts
@@ -9,14 +9,9 @@ import {Component, View, NgFor, bootstrap} from 'angular2/angular2';
 })
 class Socially {
   constructor () {
-    this.parties = [
-      {'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.'},
-      {'name': 'All dubstep all the time',
-        'description': 'Get it on!'},
-      {'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.'}
-    ];
+    Tracker.autorun(zone.bind(() => {
+      this.parties = Parties.find().fetch();
+    }));
   }
 }
 
