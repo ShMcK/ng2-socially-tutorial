206924ba42aa74f5147f0a13f5b39eaf7660d367
ShMcK <me@shmck.com>
2015-08-23 11:00:49 +0900

    tutorial setup

--- packages/ng2-socially-tutorial/client/registerTutorial.js
+++ packages/ng2-socially-tutorial/client/registerTutorial.js
@@ -2,10 +2,3 @@ Diffbox.registerTutorial('tutorialAngular2', {
   githubRepoName: 'shmck/ng2-socially-tutorial',
   patchFilename: 'angular-meteor2.multi.patch'
 });
-
-//TutorialRegistry.registerTutorial({
-//  title: 'Meteor Angular2 Tutorial',
-//  subtitle: 'Socially',
-//  tutorialSourceLink: 'angular-meteor.com/tutorials/angular2/bootstrapping',
-//  steps: []
-//});
