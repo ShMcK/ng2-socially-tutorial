97fbe943a4f7f0f473e08791e74939f8f8d8a45a
ShMcK <me@shmck.com>
2015-08-23 19:07:53 +0900

    Step 4.5: Import form directives and add them to the list of parties-form view directives

--- client/parties-form/parties-form.ts
+++ client/parties-form/parties-form.ts
@@ -1,9 +1,11 @@
 import {Component, View} from 'angular2/angular2';
+import {FORM_DIRECTIVES, Control, ControlGroup, Validators} from 'angular2/angular2';
 
 @Component({
   selector: 'parties-form'
 })
 @View({
-  templateUrl: 'client/parties-form/parties-form.ng.html'
+  templateUrl: 'client/parties-form/parties-form.ng.html',
+  directives: [FORM_DIRECTIVES]
 })
 export class PartiesForm {}
