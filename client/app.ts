f52a3c9ae2da41a91e67f5a91fb0fb7cad9c1ff3
ShMcK <me@shmck.com>
2015-08-27 08:42:38 +0900

    Step 5.5: Cleanup the root app, change the template to use <router-outlet>

--- client/app.ts
+++ client/app.ts
@@ -1,24 +1,14 @@
-import {Component, View, NgFor, bootstrap} from 'angular2/angular2';
+import {Component, View, bootstrap} from 'angular2/angular2';
 import {routerInjectables, RouterOutlet, RouterLink, RouteConfig} from 'angular2/router';
-import {PartiesForm} from 'client/parties-form/parties-form';
 
 @Component({
   selector: 'app'
 })
 @View({
-  templateUrl: 'client/index.ng.html',
-  directives: [NgFor, PartiesForm, RouterOutlet, RouterLink]
+  template: '<router-outlet></router-outlet>',
+  directives: [RouterOutlet, RouterLink]
 })
 @RouteConfig()
-class Socially {
-  constructor () {
-    Tracker.autorun(zone.bind(() => {
-      this.parties = Parties.find().fetch();
-    }));
-  }
-  remove(party) {
-    Parties.remove(party._id);
-  }
-}
+class Socially {}
 
 bootstrap(Socially, [routerInjectables]);
