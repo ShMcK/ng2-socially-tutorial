b4c37504da8a42feb8bc0678c42660059c83fd38
ShMcK <me@shmck.com>
2015-08-27 08:39:15 +0900

    Step 5.4: Move root app into a parties-list component

---
+++ client/parties-list/parties-list.ts
@@ -0,0 +1,20 @@
+import {Component, View, NgFor, bootstrap} from 'angular2/angular2';
+import {PartiesForm} from 'client/parties-form/parties-form';
+
+@Component({
+  selector: 'parties-list'
+})
+@View({
+  templateUrl: 'client/parties-list/parties-list.ng.html',
+  directives: [NgFor, PartiesForm]
+})
+export class PartiesList {
+  constructor () {
+    Tracker.autorun(zone.bind(() => {
+      this.parties = Parties.find().fetch();
+    }));
+  }
+  remove(party) {
+    Parties.remove(party._id);
+  }
+}
