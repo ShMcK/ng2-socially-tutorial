ab07423378864ad01534fc09d9f1c248ed4c355a
ShMcK <me@shmck.com>
2015-08-27 08:49:20 +0900

    Step 5.8: Create PartyDetails component

---
+++ client/party-details/party-details.ts
@@ -0,0 +1,9 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'party-details'
+})
+@View({
+  templateUrl: 'client/party-details/party-details.ng.html'
+})
+export class PartyDetails {}
