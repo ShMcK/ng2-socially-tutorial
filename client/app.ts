062f352bd8867e3627802df5d009f1bcf9de9093
ShMcK <me@shmck.com>
2015-08-27 09:03:27 +0900

    Step 5.10: Configure routes in app.ts

--- client/app.ts
+++ client/app.ts
@@ -2,6 +2,10 @@ import {Component, View, NgFor, bootstrap} from 'angular2/angular2';
 import {ROUTER_BINDINGS, ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 import {PartiesForm} from 'client/parties-form/parties-form';
 
+// Components
+import {PartiesList} from 'client/parties-list/parties-list';
+import {PartyDetails} from 'client/party-details/party-details';
+
 @Component({
   selector: 'app'
 })
@@ -9,7 +13,11 @@ import {PartiesForm} from 'client/parties-form/parties-form';
   templateUrl: 'client/index.ng.html',
   directives: [NgFor, PartiesForm, ROUTER_DIRECTIVES]
 })
-@RouteConfig()
+@RouteConfig([
+  {path: '/', redirectTo: '/parties'},
+  {path: '/parties',  as: 'parties-list', component: PartiesList},
+  {path: '/party/:partyId', as: 'party-details', component: PartyDetails}
+])
 class Socially {}
 
 bootstrap(Socially, [ROUTER_BINDINGS]);
