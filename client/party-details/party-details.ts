0d54028256d91eae9b653dcceee4db25071a7c2f
ShMcK <me@shmck.com>
2015-08-27 09:50:38 +0900

    Step 6.3: import FORM_DIRECTIVES into party-details and add it to the directive view

--- client/party-details/party-details.ts
+++ client/party-details/party-details.ts
@@ -1,12 +1,13 @@
 import {Component, View, Inject} from 'angular2/angular2';
 import {RouteParams, RouterLink} from 'angular2/router';
+import {FORM_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
   selector: 'party-details'
 })
 @View({
   templateUrl: 'client/party-details/party-details.ng.html',
-  directives: [RouterLink]
+  directives: [RouterLink, FORM_DIRECTIVES]
 })
 export class PartyDetails {
   constructor(@Inject(RouteParams) routeParams:RouteParams) {
