cd97fbb7fbf3b65c4cfe652bf7568a4f06a9bb97
ShMcK <me@shmck.com>
2015-08-27 13:39:21 +0900

    Step 8.3: Create security rules for insert, update & removing Parties

--- model/parties.ts
+++ model/parties.ts
@@ -1,1 +1,16 @@
 Parties = new Mongo.Collection('parties');
+
+Parties.allow({
+  insert: function (party:IParty) {
+    var userId = Meteor.userId();
+    return userId && party.owner === userId;
+  },
+  update: function (party:IParty, fields, modifier) {
+    var userId = Meteor.userId();
+    return userId && party.owner === userId;
+  },
+  remove: function (party:IParty) {
+    var userId = Meteor.userId();
+    return userId && party.owner === userId;
+  }
+});
