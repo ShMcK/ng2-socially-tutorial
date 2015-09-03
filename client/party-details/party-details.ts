673342e470d3c37ba31b79e2c52d409db4da8653
ShMcK <shawn.j.mckay@gmail.com>
2015-09-03 21:15:15 +0900

    Step 6.6: Save party-details on submit from component, update db

--- client/party-details/party-details.ts
+++ client/party-details/party-details.ts
@@ -13,6 +13,15 @@ export class PartyDetails {
   constructor(@Inject(RouteParams) routeParams:RouteParams) {
     this.partyId = routeParams.params.partyId;
   }
+  save(event) {
+    event.preventDefault();
+    var party = this.party;
+
+    Parties.update(party._id, {
+        name: party.name,
+        description: party.description
+    });
+  }
   onActivate() {
     this.party = Parties.find(this.partyId).fetch()[0];
     if (this.party) {
