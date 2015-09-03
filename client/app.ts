1cd938385d2c2a2a005c417f8b0ad057d8884649
ShMcK <shawn.j.mckay@gmail.com>
2015-09-03 13:51:27 +0900

    Step 5.2: Import Router dependencies, add them to View directives, and init RouteConfig

--- client/app.ts
+++ client/app.ts
@@ -1,4 +1,5 @@
 import {Component, View, NgFor, bootstrap} from 'angular2/angular2';
+import {ROUTER_BINDINGS, ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 import {PartiesForm} from 'client/parties-form/parties-form';
 
 @Component({
@@ -6,8 +7,9 @@ import {PartiesForm} from 'client/parties-form/parties-form';
 })
 @View({
   templateUrl: 'client/index.ng.html',
-  directives: [NgFor, PartiesForm]
+  directives: [NgFor, PartiesForm, ROUTER_DIRECTIVES]
 })
+@RouteConfig()
 class Socially {
   constructor () {
     Tracker.autorun(zone.bind(() => {
@@ -19,4 +21,4 @@ class Socially {
   }
 }
 
-bootstrap(Socially);
+bootstrap(Socially, [ROUTER_BINDINGS]);
