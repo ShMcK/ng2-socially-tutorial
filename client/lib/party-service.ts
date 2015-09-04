b94994d83392968a8435ab8483762027482a8060
ShMcK <shawn.j.mckay@gmail.com>
2015-09-04 18:25:09 +0900

    Step 8.10: create add, update, & remove methods on PartyService

--- client/lib/party-service.ts
+++ client/lib/party-service.ts
@@ -1,7 +1,13 @@
 export class PartyService {
-  add(party:IParty) { }
-  update (party:IParty) { }
-  remove(partyId:string) { }
+  add(party:IParty) {
+    Parties.insert(this.getPartyObject(party));
+  }
+  update (party:IParty) {
+    Parties.update(party._id, this.getPartyObject(party));
+  }
+  remove(partyId:string) {
+    Parties.remove(partyId);
+  }
   getPartyObject(party:IParty) {
     return {
       name: party.name,
