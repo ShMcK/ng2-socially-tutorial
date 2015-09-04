8270aa019f2bde63304c3f627ec82efad9cc7533
ShMcK <shawn.j.mckay@gmail.com>
2015-09-04 18:24:10 +0900

    Step 8.9: create getPartyObject in PartyService

--- client/lib/party-service.ts
+++ client/lib/party-service.ts
@@ -2,4 +2,11 @@ export class PartyService {
   add(party:IParty) { }
   update (party:IParty) { }
   remove(partyId:string) { }
+  getPartyObject(party:IParty) {
+    return {
+      name: party.name,
+      description: party.description,
+      user: Meteor.userId()
+    };
+  }
 }
