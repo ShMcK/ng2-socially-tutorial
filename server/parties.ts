67acd1b6577b12eccd5d2f51b5c90193a74ca977
ShMcK <shawn.j.mckay@gmail.com>
2015-09-10 16:06:14 +0900

    Step 9.4: publish only parties that are public or owned by the user

--- server/parties.ts
+++ server/parties.ts
@@ -1,3 +1,13 @@
 Meteor.publish('parties', function () {
-  return Parties.find();
+  return Parties.find({
+    $or:[
+      {$and:[
+        {"isPublic": true},
+        {"isPublic": {$exists: true}}
+      ]},
+      {$and:[
+        {owner: this.userId},
+        {owner: {$exists: true}}
+      ]}
+    ]});
 });
