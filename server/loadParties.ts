e61ae7d20c159cd84a025b1cb2d722a8af446159
ShMcK <shawn.j.mckay@gmail.com>
2015-09-10 16:07:25 +0900

    Step 9.5: load initial parties with owner and isPublic values

--- server/loadParties.ts
+++ server/loadParties.ts
@@ -1,14 +1,22 @@
 Meteor.startup(function () {
   if (Parties.find().count() === 0) {
 
-    var parties:IParty[] = [
-      {'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.'},
-      {'name': 'All dubstep all the time',
-        'description': 'Get it on!'},
-      {'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.'}
-    ];
+    var parties:IParty[] = [{
+      name: 'Dubstep-Free Zone',
+      description: 'Can we please just for an evening not listen to dubstep.',
+      owner: 'anonymous',
+      isPublic: true
+    }, {
+      name: 'All dubstep all the time',
+      description: 'Get it on!',
+      owner: 'anonymous',
+      isPublic: true
+    }, {
+      name: 'Savage lounging',
+      description: 'Leisure suit required. And only fiercest manners.',
+      owner: 'anonymous',
+      isPublic: false
+    }];
 
     for (var i = 0; i < parties.length; i++)
       Parties.insert(parties[i]);
